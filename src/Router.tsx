4f2324bfe1a51c382b201b992e90a15515b979e1
Gabriel Brito Oliveira <gabrielbrito.dev@gmail.com>
2022-10-10 23:12:59 -0300

    fix: alter component name client to customer

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layout/DefaultLayout";
 import { Counter } from "./pages/Counter";
-import { Client } from "./pages/Client";
+import { Customer } from "./pages/Customer";
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 
@@ -10,7 +10,7 @@ export function Router() {
     <Routes>
       <Route path="/lmcontabilidade" element={<DefaultLayout />}>
         <Route path="/lmcontabilidade/painel/counter" element={<Counter />} />
-        <Route path="/lmcontabilidade/painel/client" element={<Client />} />
+        <Route path="/lmcontabilidade/painel/customer" element={<Customer />} />
         <Route path="/lmcontabilidade/login" element={<Login />} />
         <Route path="/lmcontabilidade/register" element={<Register />} />
       </Route>
