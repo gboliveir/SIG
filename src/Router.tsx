d2de5e779a1666b816bf4d7368ca065ce801269a
Gabriel Brito Oliveira <gabrielbrito.dev@gmail.com>
2022-10-10 23:40:58 -0300

    adjust shifts

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from "react-router-dom";
+
 import { DefaultLayout } from "./layout/DefaultLayout";
+import { Home } from "./pages/Home";
 import { Counter } from "./pages/Counter";
 import { Customer } from "./pages/Customer";
 import { Login } from "./pages/Login";
@@ -8,11 +10,14 @@ import { Register } from "./pages/Register";
 export function Router() {
   return (
     <Routes>
-      <Route path="/lmcontabilidade" element={<DefaultLayout />}>
-        <Route path="/lmcontabilidade/painel/counter" element={<Counter />} />
-        <Route path="/lmcontabilidade/painel/customer" element={<Customer />} />
+      <Route path="/lmcontabilidade">
+        <Route path="/lmcontabilidade/home" element={<Home />} />
         <Route path="/lmcontabilidade/login" element={<Login />} />
         <Route path="/lmcontabilidade/register" element={<Register />} />
+        <Route path="/lmcontabilidade/painel/" element={<DefaultLayout />}>
+          <Route path="/lmcontabilidade/painel/counter" element={<Counter />} />
+          <Route path="/lmcontabilidade/painel/customer" element={<Customer />} />
+        </Route>
       </Route>
     </Routes>
   )
