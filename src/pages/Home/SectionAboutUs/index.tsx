1eafd5954141ee60db1b9e858ca6b8cbdfe2bc7e
Gabriel Brito Oliveira <gabrielbrito.dev@gmail.com>
2022-10-17 21:30:24 -0300

    chore: add section elemento to section about us component

--- src/pages/Home/SectionAboutUs/index.tsx
+++ src/pages/Home/SectionAboutUs/index.tsx
@@ -3,19 +3,21 @@ import { CarouselItem } from "../CarouselItem";
 
 export function SectionAboutUs() {
   return (
-    <Carousel style={{ height: '100vh', background: '#364d79' }}>
-      <CarouselItem 
-        title="Conheça a LM Contabilidade"
-        description="Somos o que forma a razão social L desde 1992. Uma empresa de serviços contábeis, fiscais e de recursos humanos!"
-      />
-      <CarouselItem 
-        title="Seus profissionais"
-        description="A equipe tem uma vasta experiência com os serviços fiscais e contábeis, estando em constante atualização conforme as transformações na legislação vigente. Conta com um suporte de informações inteligentes que atua como facilitadora em diversas atividades, sejam elas econômicas e/ou tributárias."
-      />
-      <CarouselItem 
-        title="Onde esta localizada?"
-        description="A empresa tem apenas uma sede localizada na rua xxxxx, nº xxxxx, centro, xx – xx."
-      />
-    </Carousel>
+    <section>
+      <Carousel style={{ height: '100vh', background: '#364d79' }}>
+        <CarouselItem 
+          title="Conheça a LM Contabilidade"
+          description="Somos o que forma a razão social L desde 1992. Uma empresa de serviços contábeis, fiscais e de recursos humanos!"
+        />
+        <CarouselItem 
+          title="Seus profissionais"
+          description="A equipe tem uma vasta experiência com os serviços fiscais e contábeis, estando em constante atualização conforme as transformações na legislação vigente. Conta com um suporte de informações inteligentes que atua como facilitadora em diversas atividades, sejam elas econômicas e/ou tributárias."
+        />
+        <CarouselItem 
+          title="Onde esta localizada?"
+          description="A empresa tem apenas uma sede localizada na rua xxxxx, nº xxxxx, centro, xx – xx."
+        />
+      </Carousel>
+    </section>
   );
 }
