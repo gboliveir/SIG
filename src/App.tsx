484c8e2ab277758c0848054293096890fb7909ed
Gabriel Brito Oliveira <gabrielbrito.dev@gmail.com>
2022-11-06 15:58:07 -0300

    chore: config provider to locale configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './Router';
 
+import { ConfigProvider } from 'antd';
+import ptBR from 'antd/es/locale/pt_BR';
 import { AuthProvider } from './contexts/AuthContext';
 import { UserEditingDrawerProvider } from './contexts/UserEditingDrawerContext';
 import { UserDocumentationsDrawerProvider } from './contexts/UserDocumentationsDrawerContext';
@@ -10,14 +12,16 @@ import 'antd/dist/antd.css';
 
 export function App() {
   return (
-    <AuthProvider>
-      <UserEditingDrawerProvider>
-        <UserDocumentationsDrawerProvider>
-          <BrowserRouter>
-            <Router />
-          </BrowserRouter>
-        </UserDocumentationsDrawerProvider>
-      </UserEditingDrawerProvider>
-    </AuthProvider>
+    <ConfigProvider locale={ptBR}>
+      <AuthProvider>
+        <UserEditingDrawerProvider>
+          <UserDocumentationsDrawerProvider>
+            <BrowserRouter>
+              <Router />
+            </BrowserRouter>
+          </UserDocumentationsDrawerProvider>
+        </UserEditingDrawerProvider>
+      </AuthProvider>
+    </ConfigProvider>
   )
 }
